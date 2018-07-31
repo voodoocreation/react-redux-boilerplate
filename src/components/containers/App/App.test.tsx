f5ed9d64706d63cb8a7c693c2ad3f6989ebd1baa
Raice Hannay <raice.hannay@xero.com>
2018-07-31 15:33:36 +1200

    move App component to src for better visibility + add App coverage

---
+++ src/components/containers/App/App.test.tsx
@@ -0,0 +1,120 @@
+import { mount } from "enzyme";
+import merge from "lodash.merge";
+import * as React from "react";
+
+import App from "./App";
+
+jest.mock("../../../../next.routes", () => ({
+  Router: {
+    route: ""
+  }
+}));
+
+import routes from "../../../../next.routes";
+
+const setup = async (fn: any, fromTestProps?: any) => {
+  const Component = () => <div className="PageComponent" />;
+  const appProps = merge({
+      Component,
+      asPath: "",
+      ctx: {
+        isServer: false,
+        pathname: "",
+        query: {},
+        req: {
+          intlMessages: {},
+          locale: "en-NZ"
+        },
+        res: {},
+      },
+      router: {
+        pathname: ""
+      }
+    },
+    fromTestProps
+  );
+  const initialProps = await App.getInitialProps(appProps);
+  const props = {
+    ...appProps,
+    ...initialProps,
+  };
+
+  return {
+    actual: fn(<App {...props} />),
+    props
+  }
+};
+
+const g: any = global;
+
+describe("[app] App root", () => {
+  beforeEach(() => {
+    g.isServer = false;
+    g.__NEXT_DATA__ = {
+      props: {
+        initialProps: {
+          intlProps: {}
+        }
+      }
+    };
+  });
+
+  afterEach(() => {
+    g.__NEXT_DATA__ = undefined;
+  });
+
+  it("mounts application correctly on the server", async () => {
+    g.isServer = true;
+    const { actual } = await setup(mount, {
+      ctx: { isServer: g.isServer }
+    });
+
+    expect(actual.render()).toMatchSnapshot();
+  });
+
+  it("mounts application correctly on the client", async () => {
+    const { actual } = await setup(mount);
+
+    expect(actual.render()).toMatchSnapshot();
+  });
+
+  it("gets `initialProps` from component correctly", async () => {
+    const test = "Test";
+    const Component: any = () => <div className="PageComponent" />;
+    Component.getInitialProps = async () => ({ test });
+    const { props } = await setup(mount, { Component });
+
+    expect(props.initialProps.pageProps).toEqual({ test })
+  });
+
+  describe("router events", async () => {
+    it("onRouteChangeStart is handled correctly", async () => {
+      const { props } = await setup(mount);
+
+      routes.Router.onRouteChangeStart("/");
+
+      expect(props.store.getState().page.transitioningTo).toBe("/");
+    });
+
+    it("onRouteChangeComplete is handled correctly", async () => {
+      const { props } = await setup(mount);
+
+      routes.Router.onRouteChangeComplete("/");
+
+      expect(props.store.getState().page.transitioningTo).toBeUndefined();
+      expect(props.store.getState().page.currentRoute).toBe("/");
+    });
+
+    it("onRouteChangeStart is handled correctly", async () => {
+      const { props } = await setup(mount);
+
+      routes.Router.onRouteChangeError(new Error("Server error"), "/");
+
+      expect(props.store.getState().page.transitioningTo).toBeUndefined();
+      expect(props.store.getState().page.error).toEqual({
+        message: "Error: Server error",
+        status: 500
+      });
+    });
+  });
+});
---
+++ src/components/containers/App/App.tsx
@@ -0,0 +1,135 @@
+import withReduxSaga from "next-redux-saga";
+import withRedux from "next-redux-wrapper";
+import App, { AppComponentProps, Container } from "next/app";
+import { NextDocumentContext as Context } from "next/document";
+import * as React from "react";
+import { addLocaleData, IntlProvider } from "react-intl";
+import { Provider } from "react-redux";
+import { Store } from "redux";
+
+import routes from "../../../../next.routes";
+import * as actions from "../../../actions/root.actions";
+import createStore from "../../../store/root.store";
+
+import { isServer } from "../../../helpers/dom";
+import Shell from "../Shell/Shell";
+
+// tslint:disable:no-submodule-imports
+import en from "react-intl/locale-data/en";
+// tslint:enable:no-submodule-imports
+
+addLocaleData([...en]);
+
+type NextPageComponent = React.ComponentType<any> & {
+  getInitialProps: (props: any) => any;
+};
+
+interface IProps extends AppComponentProps {
+  Component: NextPageComponent;
+  ctx: Context & { store: any };
+  intlProps: {
+    locale: string;
+    intlMessages: {};
+    initialNow: Date;
+  };
+  store: Store<any>;
+}
+
+const getIntlProps = (ctx: Context) => {
+  const requestProps = isServer()
+    ? ctx.req
+    : window.__NEXT_DATA__.props.initialProps.intlProps;
+  const { locale, intlMessages } = requestProps;
+
+  return {
+    initialNow: Date.now(),
+    intlMessages: intlMessages || {},
+    locale: locale || "en-NZ"
+  };
+};
+
+class Application extends App {
+  public static async getInitialProps({ ctx, Component }: IProps) {
+    let pageProps = {};
+
+    const unsubscribe = ctx.store.subscribe(() => {
+      const state = ctx.store.getState();
+
+      if (state.page.error && ctx.res) {
+        ctx.res.statusCode = state.page.error.status;
+        unsubscribe();
+      }
+    });
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps({ ctx });
+    }
+
+    const intlProps = getIntlProps(ctx);
+
+    return { pageProps, intlProps };
+  }
+
+  public componentWillMount() {
+    routes.Router.onRouteChangeStart = this.onRouteChangeStart;
+    routes.Router.onRouteChangeComplete = this.onRouteChangeComplete;
+    routes.Router.onRouteChangeError = this.onRouteChangeError;
+  }
+
+  public componentDidMount() {
+    if (!isServer()) {
+      const { store } = this.props as IProps;
+      store.dispatch(actions.setCurrentRoute(routes.Router.route));
+    }
+  }
+
+  public render() {
+    const { Component, intlProps, pageProps, store } = this.props as IProps;
+
+    return (
+      <Container>
+        <Provider store={store}>
+          <IntlProvider
+            initialNow={intlProps.initialNow}
+            messages={intlProps.intlMessages}
+            locale={intlProps.locale}
+          >
+            <Shell>
+              <Component {...pageProps} />
+            </Shell>
+          </IntlProvider>
+        </Provider>
+      </Container>
+    );
+  }
+
+  private onRouteChangeStart = (path: string) => {
+    const { store } = this.props as IProps;
+
+    store.dispatch(actions.changeRoute.started(path));
+  };
+
+  private onRouteChangeComplete = (path: string) => {
+    const { store } = this.props as IProps;
+
+    store.dispatch(actions.changeRoute.done({ params: path }));
+  };
+
+  private onRouteChangeError = (error: Error, path: string) => {
+    const { store } = this.props as IProps;
+
+    store.dispatch(
+      actions.changeRoute.failed({
+        error: {
+          message: error.toString(),
+          status: 500
+        },
+        params: path
+      })
+    );
+  };
+}
+
+export default withRedux(createStore)(
+  withReduxSaga({ async: true })(Application)
+);
