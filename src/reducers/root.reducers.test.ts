155067c2584ed5f02d790c4a557685d32a38e0ad
Raice Hannay <voodoocreation@gmail.com>
2019-06-28 11:55:49 +1200

    add root reducer test

---
+++ src/reducers/root.reducers.test.ts
@@ -0,0 +1,11 @@
+import rootReducer, { initialState } from "./root.reducers";
+
+describe("[reducers] Root", () => {
+  it("combines all reducers correctly", () => {
+    const state = rootReducer({} as any, {
+      type: "NOTHING"
+    });
+
+    expect(state).toEqual(initialState);
+  });
+});
